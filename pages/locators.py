a4ea5df3e35748851e32b9b98785ddbd478bb6a4
oleg-yaschenia <yaschenia@gmail.com>
2022-10-14 19:35:24 +0300

    moving login locators to basepage, moving MainPage tests to BasePage and creating the same tests for Product Page

--- pages/locators.py
+++ pages/locators.py
@@ -1,8 +1,8 @@
 from selenium.webdriver.common.by import By
 
-
-class MainPageLocators():
+class BasePageLocators():
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+    LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
 
 class LoginPageLocators():
     LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
@@ -12,8 +12,8 @@ class ProductPageLocators():
     ADD_TO_BASKET = (By.CSS_SELECTOR, ".btn-add-to-basket")
     PRODUCT_PRICE = (By.CSS_SELECTOR, "p.price_color")
     PRODUCT_NAME = (By.CSS_SELECTOR, ".col-sm-6.product_main h1")
-    ADDED_PRODUCT_PRICE = (By.CSS_SELECTOR, "[class ='alertinner '] > p strong")
-    ADDED_PRODUCT_NAME = (By.XPATH, "(//div[@class ='alertinner ']/strong)[1]")
+    SUCCESS_PRODUCT_PRICE = (By.CSS_SELECTOR, "[class ='alertinner '] > p strong")
+    SUCCESS_PRODUCT_NAME = (By.XPATH, "(//div[@class ='alertinner ']/strong)[1]")
 
 
 
--- pages/product_page.py
+++ pages/product_page.py
@@ -9,7 +9,7 @@ class ProductPage(BasePage):
 
     def comparing_product_names(self):
         product_name = self.browser.find_element(*ProductPageLocators.PRODUCT_NAME).text
-        added_product_name = self.browser.find_element(*ProductPageLocators.ADDED_PRODUCT_NAME).text
+        added_product_name = self.browser.find_element(*ProductPageLocators.SUCCESS_PRODUCT_NAME).text
         assert product_name == added_product_name,\
             f"Added product name is not correct. " \
             f"Expected: {product_name}, " \
@@ -17,9 +17,16 @@ class ProductPage(BasePage):
 
     def comparing_product_prices(self):
         product_price = self.browser.find_element(*ProductPageLocators.PRODUCT_PRICE).text
-        added_product_price = self.browser.find_element(*ProductPageLocators.ADDED_PRODUCT_PRICE).text
+        added_product_price = self.browser.find_element(*ProductPageLocators.SUCCESS_PRODUCT_PRICE).text
         assert product_price == added_product_price,\
             f"Added product price is not correct. " \
             f"Expected: {product_price}, " \
             f"actual: {added_product_price}"
 
+    def should_not_be_success_message(self):
+        assert self.is_not_element_present(*ProductPageLocators.SUCCESS_PRODUCT_NAME), \
+           "Success message is presented, but should not be"
+
+    def should_disappear_success_message(self):
+        assert self.is_disappeared(*ProductPageLocators.SUCCESS_PRODUCT_NAME), \
+           "Success message is presented, but should not be"
